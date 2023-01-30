e39065d8fa5e30ee2259310bcf1b993a17fcf49e
c+1 <89845610+s-over-4@users.noreply.github.com>
2023-01-30 13:08:19 -0500

    added templates for the future

--- index.php
+++ index.php
@@ -20,5 +20,34 @@
             <i>dedicated to preserving the bots of <a class="textLink" href="https://euphoria.io/">euphoria</a> since 2023</i>
          </div>
       </div>
+
+      <div class="textBox">
+         <div class="textHeader">
+            search for a bot
+         </div>
+         <div class="textBody">
+            <form class="form">
+               <div class="formObject">
+                  search with...<br>
+                  <input type="text" class="inputText" placeholder="bot name...">
+                  <input type="text" class="inputText" placeholder="bot creator...">
+                  <input type="text" class="inputText" placeholder="bot lifespan...">
+               </div>
+            </form>
+         </div>
+      </div>
+
+      <div class="textBox" style="flex-grow:1">
+         <div class="textHeader">
+            elsewhere:
+         </div>
+         <div class="textBody">
+            - <a class="textLink" href="https://github.com/s-over-4/wiki.nest.place">this wiki on github</a><br>
+            - <a class="textLink" href="https://github.com/euphoria-io/heim">euphoria on github</a><br>
+            - <a class="textLink" href="https://euphoria.io">only (known) euphoria instance</a><br>
+            - <a class="textLink" href="https://nest.place">main page</a>
+         </div>
+      </div>
+   </div>
 </body>
 </html>
