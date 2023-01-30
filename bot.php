ba8cca0101b77630e58e7a54a837c10b96c3f3e4
c+1 <89845610+s-over-4@users.noreply.github.com>
2023-01-30 17:30:14 -0500

    some test stuff to make sure I still know how php works

--- bot.php
+++ bot.php
@@ -1,5 +1,15 @@
 <?php
 
+$bot = $_GET["bot"];
+echo "bot: " . $bot;
 
+$path = "bots/{$bot}.json";
+echo "<br><br>path: " . $path;
 
+$jsonRaw = file_get_contents($path);
+echo "<br><br>raw file contents: " . $jsonRaw;
+
+$jsonData = json_decode($jsonRaw, true);
+echo "<br><br>php array: ";
+print_r($jsonData);
 ?>
