00e6a556234764e3f773e91853aeb04a0ab4f094
c+1 <89845610+s-over-4@users.noreply.github.com>
2023-01-30 13:33:48 -0500

    minor

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link rel="stylesheet" href="nest.place/css/style.css">
-   <link rel="stylesheet" href="nest.place/css/index.css">
+   <link rel="stylesheet" href="https://nest.place/css/style.css">
+   <link rel="stylesheet" href="https://nest.place/css/index.css">
    <title>the euphoria bot wiki</title>
 </head>
 
