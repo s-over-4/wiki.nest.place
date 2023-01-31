12ddc40f6694ed709b669662e2a22f4570521a6e
c+1 <89845610+s-over-4@users.noreply.github.com>
2023-01-31 09:47:23 -0500

    bot info page first draft

--- bot.php
+++ bot.php
@@ -1,15 +1,61 @@
 <?php
 
 $bot = $_GET["bot"];
-echo "bot: " . $bot;
-
 $path = "bots/{$bot}.json";
-echo "<br><br>path: " . $path;
-
 $jsonRaw = file_get_contents($path);
-echo "<br><br>raw file contents: " . $jsonRaw;
-
 $jsonData = json_decode($jsonRaw, true);
-echo "<br><br>php array: ";
-print_r($jsonData);
+
+
+/* TESTING */
+//echo "bot: " . $bot;
+//echo "<br><br>path: " . $path;
+//echo "<br><br>raw file contents: " . $jsonRaw;
+// echo "<br><br>php array: ";
+// print_r($jsonData);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="https://nest.place/css/style.css">
+   <link rel="stylesheet" href="https://nest.place/css/index.css">
+   <title><?php echo $bot; ?></title>
+</head>
+
+<body>
+   <div id="tempBoxHolder">
+      <div class="textBox" id="thickBox">
+         <div class="textHeader">
+            <?php echo $bot; ?>
+         </div>
+         <div class="textBody">
+            <pre>
+status:<?php        echo "\t\t" . $jsonData["status"] . "\n\n"; ?>
+aliases:<?php       echo "\t" . join(",\n\t\t", $jsonData["aliases"]) . "\n\n"; ?>
+creators:<?php      echo "\t" . join(",\n\t\t", $jsonData["creators"]) . "\n\n"; ?>
+year created:<?php  echo "\t" . $jsonData["created"] . "\n\n"; ?>
+sources:<?php       echo "\t" . join(",\n\t\t", $jsonData["sources"]) . "\n\n"; ?>
+active rooms:<?php  echo "\t" . join(",\n\t\t", $jsonData["activeRooms"]) . "\n\n"; ?>
+            </pre>
+         </div>
+      </div>
+
+      <div class="textBox" id="smallBox">
+         <div class="textHeader">
+            about
+         </div>
+         <div class="textBody">
+<?php echo $jsonData["description"]; ?>
+            <br><br>
+            <b>timeline:</b>
+            <br>
+<?php echo join("<br>", $jsonData["timeline"]) . "\n"; ?>   
+         </div>
+      </div>
+   </div>
+</body>
+</html>
